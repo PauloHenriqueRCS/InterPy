216bf69f23dd5e9f2cb7d630cfeff4aedf37d16f
Paulo <paulohenriquercs@gmail.com>
2019-08-25 18:12:11 -0300

    alterando etrutura e defs

--- src/baseModules/fileread.py
+++ src/baseModules/fileread.py
@@ -1,7 +1,21 @@
 import io
 
-
 def fileread(filepath):
+    try:
+        fstr = []
+        filecontent = open(filepath, mode="r", encoding="utf-8")
+        for line in filecontent:
+            if line.find("//") == -1:
+                fstr.append(line)
+            else:
+                continue
+        return fstr
+    except IOError as identifier:
+        print(str(identifier))
+    finally:
+        filecontent.close()
+
+def filereadalt(filepath):
     try:
         filecontent = io.open(filepath, mode="r", encoding="utf-8").read()
         return filecontent
--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-import sys
+import io
+import re
 
-from src.baseModules import *
+
+def fileread(filepath):
+    try:
+        fstr = []
+        filecontent = open(filepath, mode="r", encoding="utf-8")
+        for line in filecontent:
+            if line.find("//") == -1:
+                fstr.append(line)
+            else:
+                continue
+        return fstr
+    except IOError as identifier:
+        print(str(identifier))
+    finally:
+        filecontent.close()
+
 
 if __name__ == "__main__":
-    filestr = fileread("../outFiles/teste.xul")
+    filestr = fileread("outFiles/teste.xul")
+    print(filestr)
