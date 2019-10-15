d5c0cdc2751e347acc107670bc67d350e4ce8533
Paulo <paulohenriquercs@gmail.com>
2019-10-15 16:20:11 -0300

    lexical detect text in string

--- src/packModules/lexical.py
+++ src/packModules/lexical.py
@@ -3,13 +3,18 @@ import re
 
 class Lexical:
     def __init__(self):
+        self.lexeme = ''
+        self.tokenslist = []
+        self.inText = False
         self.symbols = re.compile('\(|\)|\{|\}|\[|\]|\;')
         self.keywords = re.compile(
-            'int|float|double|True|False|bool|if|else|for|while|break|return')
+            'int|float|double|string|True|False|bool|if|else|for|while|break|return')
         self.id = re.compile('[a-zA-Z0-9_]+')
         self.logic = re.compile('(=|>=|<=|>|<|==|!=|\+|\-|\*|\/)')
         self.decimal = re.compile('[0-9]+[.][0-9]+|[0-9]+[.]|[.][0-9]+')
         self.digit = re.compile('[0-9]+')
+        self.text = re.compile('([\w])+')
+        self.aspas = re.compile('\"|\'')
         self.symbolsdict = {
             '{': '{',
             '}': '}',
@@ -20,7 +25,7 @@ class Lexical:
             'int': 'int',
             'float': 'float',
             'bool': 'bool',
-            'str': 'str',
+            'string': 'string',
             'double': 'double',
             'True': 'True',
             'False': 'False',
@@ -43,46 +48,74 @@ class Lexical:
             ';': ';',
         }
 
-    def validating(self, content):
-        lexeme = ''
-        tokensList = []
+    def issymbol(self, lexeme):
+        return True if self.symbols.search(lexeme) is not None else False
 
+    def iskeyword(self, lexeme):
+        return True if self.keywords.search(lexeme) is not None else False
+
+    def isdigit(self, lexeme):
+        return True if self.digit.search(lexeme) is not None else False
+
+    def isdecimal(self, lexeme):
+        return True if self.decimal.search(lexeme) is not None else False
+
+    def isid(self, lexeme):
+        return True if self.id.search(lexeme) is not None else False
+
+    def islogic(self, lexeme):
+        return True if self.logic.search(lexeme) is not None else False
+
+    def isdelimiter(self, lexeme):
+        return True if lexeme is '@' else False
+
+    def istext(self, lexeme):
+        return True if self.aspas.search(lexeme) is not None else False
+    
+    def addtoken(self, symdict = 'id'):
+        self.tokenslist.append(dict({self.symbolsdict[symdict]: self.lexeme}))
+        self.lexeme = ''
+
+    def validating(self, content):
         for lex in range(0, len(content)):
-            if content[lex] is not '@':
-                is_symbol = self.symbols.search(content[lex])
-                if not is_symbol:
-                    lexeme += content[lex]
-                    if self.keywords.search(lexeme) is not None and (content[lex + 1] is '@' or self.symbols.search(content[lex + 1])):
-                        word = self.keywords.search(lexeme)
-                        tokensList.append(
-                            dict({self.symbolsdict[word.group()]: word.group()}))
-                        lexeme = ''
-                        continue
-                    elif (self.digit.search(lexeme) is not None or self.decimal.search(lexeme) is not None) and ((self.symbols.search(content[lex + 1]) is not None) or (self.logic.search(content[lex + 1]) is not None)):
-                        tokensList.append(
-                            dict({self.symbolsdict['id']: lexeme}))
-                        lexeme = ''
-                        continue
-                    elif self.id.search(lexeme) is not None:
-                        if (content[lex + 1] is self.symbolsdict[';']) or (content[lex + 1] is '@' and self.logic.search(content[lex + 2]) is not None) or (self.logic.search(content[lex + 1]) is not None) or (self.symbols.search(content[lex + 1]) is not None) or content[lex + 1] is '@':
-                            word = self.id.search(lexeme)
-                            tokensList.append(
-                                dict({self.symbolsdict['id']: word.group()}))
-                            lexeme = ''
+            if self.isdelimiter(content[lex]) is False:
+                self.lexeme += content[lex]
+                if self.issymbol(self.lexeme) is False:
+                    if self.istext(content[lex]):
+                        self.inText = True if self.inText is False else False
+                        if self.inText is False:
+                            self.lexeme = self.lexeme.replace('"', "", 2)
+                            self.addtoken()
+                            self.inText = False
                             continue
-                    elif self.logic.search(lexeme) is not None and self.logic.search(content[lex + 1]) is None:
-                        word = self.logic.search(lexeme)
-                        tokensList.append(
-                            dict({self.symbolsdict[word.group()]: word.group()}))
-                        lexeme = ''
-                        continue
-                    else:
-                        raise ValueError(
-                            "token <{}> not a symbol.\n".format(lexeme))
+                    elif self.istext(self.lexeme) is False and self.inText is False:
+                        if self.iskeyword(self.lexeme) and (self.isdelimiter(content[lex + 1]) or self.issymbol(content[lex + 1])):
+                            self.addtoken(self.lexeme)
+                            continue
+                        elif ((self.isdigit(self.lexeme)) or (self.isdecimal(self.lexeme) )) and ((self.issymbol(content[lex + 1])) or (self.islogic(content[lex + 1])) or (self.isid(content[lex + 1]) is False)):
+                            if content[lex + 1] != '.':
+                                self.addtoken()
+                                continue
+                        elif self.isid(self.lexeme):
+                            if ((self.issymbol(content[lex + 1])) or self.isdelimiter(content[lex + 1]) ) or ((self.islogic(content[lex + 2])) or (self.islogic(content[lex + 1]))):
+                                self.addtoken()
+                                continue
+                        elif self.islogic(self.lexeme):
+                            if self.islogic(content[lex + 1]) is False:
+                                self.addtoken(self.lexeme)
+                            else:
+                                continue
+                        else:
+                            raise ValueError(
+                                "token <{}> not a symbol.\n".format(self.lexeme))
                 else:
-                    tokensList.append(
-                        (dict({self.symbolsdict[is_symbol.group()]: is_symbol.group()})))
-        return tokensList
+                    self.addtoken(self.lexeme)
+            else: 
+                if self.inText is False:
+                    self.lexeme = '' 
+                else:
+                    self.lexeme += " "
+        return self.tokenslist
 
     def tokenlistprint(self, tokenList):
         print(tokenList)
--- src/test.py
+++ src/test.py
@@ -4,6 +4,6 @@ from packModules.lexical import Lexical
 
 if __name__ == "__main__":
     lexical = Lexical()
-    tokens = fileread("outFiles/teste6.xul")
+    tokens = fileread("outFiles/teste7.xul")
     tlist = lexical.validating(tokens)
     lexical.tokenlistprint(tlist)
